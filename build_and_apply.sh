9b19d6db77270ae5713b6f3031df2441e08af094
Bart Spaans <bart.spaans@gmail.com>
2017-06-14 13:14:32 +0100

    skip_kubespec flag

--- build_and_apply.sh
+++ build_and_apply.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
-set -euxf -o pipefail
+set -euf -o pipefail
 
 deps/_/extension-docker/build_and_push_image.py "$@"
-deps/_/extension-kubespec/apply_specs.py "$@"
+
+if [ "$INPUT_skip_kubespec" = "1" ] ; then 
+    echo "Skipping kubespecs because skip_kubespec is set"
+else
+    deps/_/extension-kubespec/apply_specs.py "$@"
+fi
